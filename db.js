b745cd539ceece4779786ee778b391886b269fed
erick19921 <diegofullnet92@gmail.com>
2025-10-13 23:46:04 -0500

    api funcionando

--- db.js
+++ db.js
@@ -3,19 +3,9 @@ const { Pool } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
-let pool;
-
-// ✅ Evita crear múltiples conexiones en entorno serverless (Vercel)
-if (!global._pool) {
-  global._pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false, // 👈 Necesario para Neon
-    },
-  });
-}
-
-pool = global._pool;
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // ✅ necesario en Neon
+});
 
 export default pool;
-
